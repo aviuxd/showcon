223a1f8cabac687c793b97235acc527982e7fc21
Avi Singh <avinab.singh@gmail.com>
2017-05-01 13:11:25 -0700

    Folder deletes.

--- examples/g.js
+++
@@ -1,9 +0,0 @@
-var Glob = require("../").Glob
-
-var pattern = "../svg/*.svg"
-console.log(pattern)
-
-var mg = new Glob(pattern, {mark: true, sync:true}, function (er, matches) {
-  console.log("matches", matches)
-})
-console.log("after")
